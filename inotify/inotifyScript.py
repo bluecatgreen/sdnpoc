efba6be1d163204c10d5330ec092d4b0147800c9
Raman POC <bluecatgreen@gmail.com>
2021-06-10 16:56:22 +0530

    Added pulsar send

--- inotify/inotifyScript.py
+++ inotify/inotifyScript.py
@@ -1,6 +1,11 @@
 import inotify.adapters
+import pulsar
 
 def _main():
+ 
+    client = pulsar.Client('pulsar://localhost:6650')
+    producer  =  client.create_producer('my-topic')
+
     i = inotify.adapters.Inotify()
 
     i.add_watch('/home/VZADCLOUD/vx/stats')
@@ -10,6 +15,9 @@ def _main():
 
         print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
               path, filename, type_names))
+        if (type_names[0] == 'IN_CLOSE_WRITE'):
+           print ("Sending filename and path to pulsar")
+           producer.send(('filepath:%s:filename:%s ' %(path,filename)).encode('utf-8'))
 
 if __name__ == '__main__':
     _main()
